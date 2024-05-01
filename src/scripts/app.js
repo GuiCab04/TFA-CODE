7d639732b44d3697823d9bd7514ed7d3f5d99bee
Guillaume Cabaraux <cabaraux.guillaume@gmail.com>
2024-05-02 01:10:15 +0200

    ajout css main
    ajout css main

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -31,3 +31,38 @@ menuToggle.addEventListener('click', () => {
         body.classList.remove('overflow-hidden');
     }
 });
+
+
+
+
+
+let links = document.querySelectorAll(".linktab");
+for (let link of links) {
+    link.addEventListener("click", linkTrigger)
+}
+
+function linkTrigger(event) {
+
+    let list = event.currentTarget.closest("ul");
+    let activeLinks = list.querySelectorAll(".linktab--active")
+    for (let activeLink of activeLinks) {
+        activeLink.classList.remove("linktab--active");
+    }
+
+
+    let links = list.querySelectorAll(".linktab")
+    for (let linkTab of links) {
+        let tabId = linkTab.getAttribute("href")
+        let tabElement = document.querySelector(tabId);
+        tabElement.classList.add("hidden");
+    }
+
+    let clickedLink = event.currentTarget;
+    clickedLink.classList.add("linktab--active");
+
+    let contentId = clickedLink.getAttribute("href")
+    let content = document.querySelector(contentId);
+    content.classList.remove("hidden");
+
+}
+

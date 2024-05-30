5242605ca74e1e53327eff3d908a18dcb2fb8a73
Guillaume Cabaraux <cabaraux.guillaume@gmail.com>
2024-05-30 11:51:49 +0200

    ajout page crédit + slider

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -7,16 +7,16 @@ import ScrollTrigger from 'gsap/ScrollTrigger';
 
 gsap.registerPlugin(ScrollTrigger)
 
-
-//BURGER MENU
-
+// BURGER MENU
 
 const buttonToggle = document.querySelector('.menu--toggle');
 const menu = document.querySelector('.menu');
 const body = document.body;
 const html = document.documentElement;
 
-buttonToggle.addEventListener('click', menuOpen);
+if (buttonToggle && menu) {
+    buttonToggle.addEventListener('click', menuOpen);
+}
 
 function menuOpen() {
     menu.classList.toggle("active");
@@ -63,7 +63,7 @@ menuLinks.forEach(link => {
     });
 });
 
-//PROGRESS BAR
+// PROGRESS BAR
 
 const scrollBarProgress = document.querySelector(".progressBar");
 if (scrollBarProgress) {
@@ -77,84 +77,76 @@ function scrollListener() {
     scrollBarProgress.style.width = readPercentage + '%';
 }
 
+// STICKY NAV
 
-//STICKY NAV
+const headerSticky = document.querySelector('.header--sticky');
+if (headerSticky) {
+    headerSticky.classList.add('visible');
 
-document.querySelector('.header--sticky').classList.add('visible');
+    let lastScrollTop = 0;
 
-let lastScrollTop = 0;
+    window.addEventListener("scroll", function () {
+        let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+        if (currentScroll > lastScrollTop) {
+            headerSticky.classList.remove('visible');
+        } else {
+            headerSticky.classList.add('visible');
+        }
+        lastScrollTop = currentScroll;
+    });
+}
 
-window.addEventListener("scroll", function () {
-    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-    if (currentScroll > lastScrollTop) {
-        document.querySelector('.header--sticky').classList.remove('visible');
-    } else {
-        document.querySelector('.header--sticky').classList.add('visible');
-    }
-    lastScrollTop = currentScroll;
-});
-
-
-
-
-//MOUSE
-
-
+// MOUSE
 
 const circleElement = document.querySelector('.circle');
-const mouse = { x: 0, y: 0 };
-const previousMouse = { x: 0, y: 0 }
-const circle = { x: 0, y: 0 };
+if (circleElement) {
+    const mouse = { x: 0, y: 0 };
+    const previousMouse = { x: 0, y: 0 };
+    const circle = { x: 0, y: 0 };
 
-let currentScale = 0;
-let currentAngle = 0;
+    let currentScale = 0;
+    let currentAngle = 0;
 
-window.addEventListener('mousemove', (e) => {
-    mouse.x = e.x;
-    mouse.y = e.y;
-});
-const buttonsAndLinks = document.querySelectorAll('button, a');
-
-buttonsAndLinks.forEach(element => {
-    element.addEventListener('mouseenter', () => {
-        circleElement.classList.add('hover');
+    window.addEventListener('mousemove', (e) => {
+        mouse.x = e.x;
+        mouse.y = e.y;
     });
-    element.addEventListener('mouseleave', () => {
-        circleElement.classList.remove('hover');
+
+    const buttonsAndLinks = document.querySelectorAll('button, a');
+
+    buttonsAndLinks.forEach(element => {
+        element.addEventListener('mouseenter', () => {
+            circleElement.classList.add('hover');
+        });
+        element.addEventListener('mouseleave', () => {
+            circleElement.classList.remove('hover');
+        });
     });
-});
 
-const speed = 1;
-const tick = () => {
-    circle.x += (mouse.x - circle.x) * speed;
-    circle.y += (mouse.y - circle.y) * speed;
-    const translateTransform = `translate(${circle.x}px, ${circle.y}px)`;
-    const deltaMouseX = mouse.x - previousMouse.x;
-    const deltaMouseY = mouse.y - previousMouse.y;
-    previousMouse.x = mouse.x;
-    previousMouse.y = mouse.y;
-    const mouseVelocity = Math.min(Math.sqrt(deltaMouseX ** 2 + deltaMouseY ** 2) * 4, 150);
-    const scaleValue = (mouseVelocity / 150) * 0.5;
-    currentScale += (scaleValue - currentScale) * speed;
-    const scaleTransform = `scale(${1 + currentScale}, ${1 - currentScale})`;
-    const angle = Math.atan2(deltaMouseY, deltaMouseX) * 180 / Math.PI;
-    if (mouseVelocity > 20) {
-        currentAngle = angle;
-    }
-    const rotateTransform = `rotate(${currentAngle}deg)`;
-    circleElement.style.transform = `${translateTransform} ${rotateTransform} ${scaleTransform}`;
+    const speed = 1;
+    const tick = () => {
+        circle.x += (mouse.x - circle.x) * speed;
+        circle.y += (mouse.y - circle.y) * speed;
+        const translateTransform = `translate(${circle.x}px, ${circle.y}px)`;
+        const deltaMouseX = mouse.x - previousMouse.x;
+        const deltaMouseY = mouse.y - previousMouse.y;
+        previousMouse.x = mouse.x;
+        previousMouse.y = mouse.y;
+        const mouseVelocity = Math.min(Math.sqrt(deltaMouseX ** 2 + deltaMouseY ** 2) * 4, 150);
+        const scaleValue = (mouseVelocity / 150) * 0.5;
+        currentScale += (scaleValue - currentScale) * speed;
+        const scaleTransform = `scale(${1 + currentScale}, ${1 - currentScale})`;
+        const angle = Math.atan2(deltaMouseY, deltaMouseX) * 180 / Math.PI;
+        if (mouseVelocity > 20) {
+            currentAngle = angle;
+        }
+        const rotateTransform = `rotate(${currentAngle}deg)`;
+        circleElement.style.transform = `${translateTransform} ${rotateTransform} ${scaleTransform}`;
 
-    window.requestAnimationFrame(tick);
+        window.requestAnimationFrame(tick);
+    };
+    tick();
 }
-tick();
-
-
-
-
-
-
-
-
 
 // ETAT ACTIF NAV
 
@@ -190,18 +182,7 @@ sections.forEach((section, index) => {
     resizeObserver.observe(section);
 });
 
-
-
-
-
-
-
-
-
-
-
-//ANIM WRITE TEXT
-
+// ANIM WRITE TEXT
 
 function splitTextToSpans(element) {
     const text = element.innerText;
@@ -216,64 +197,67 @@ function splitTextToSpans(element) {
 const textMoi = document.querySelector('.home__text--moi');
 const textPortfolio = document.querySelector('.home__text--portfolio');
 
-splitTextToSpans(textMoi);
-splitTextToSpans(textPortfolio);
+if (textMoi) {
+    splitTextToSpans(textMoi);
+}
+if (textPortfolio) {
+    splitTextToSpans(textPortfolio);
+}
 
 gsap.set('.home__text span', { opacity: 0 });
 
 gsap.to('.home__text--moi span', {
     opacity: 1,
-    duration: 0.02,
-    stagger: 0.03,
+    duration: 0.01,
+    stagger: 0.01,
     ease: 'power1.inOut',
     onComplete: () => {
         gsap.to('.home__text--portfolio span', {
             opacity: 1,
-            duration: 0.02,
-            stagger: 0.03,
+            duration: 0.01,
+            stagger: 0.01,
             ease: 'power1.inOut',
         });
     }
 });
 
-
-
 // ANIM TEXT SCROLL GSAP
 
 const blockText = document.querySelector('.animated-block__text');
-const wordList = blockText.textContent.split(' ').filter(w => w.trim().length > 0);
-const spanList = wordList.map(w => '<span>' + w + '</span>');
-blockText.innerHTML = spanList.join(' ');
+if (blockText) {
+    const wordList = blockText.textContent.split(' ').filter(w => w.trim().length > 0);
+    const spanList = wordList.map(w => '<span>' + w + '</span>');
+    blockText.innerHTML = spanList.join(' ');
 
-gsap.registerPlugin(ScrollTrigger);
+    gsap.registerPlugin(ScrollTrigger);
 
-gsap.from('.animated-block span', {
-    opacity: 0.2,
-    stagger: 0.5,
-    scrollTrigger: {
-        trigger: '.animated-block',
-        start: '-10% top',
-        end: '100% top',
-        scrub: true,
-        pin: true,
-        markers: true,
-    }
-});
-
+    gsap.from('.animated-block span', {
+        opacity: 0.2,
+        stagger: 0.5,
+        scrollTrigger: {
+            trigger: '.animated-block',
+            start: '-10% top',
+            end: '100% top',
+            scrub: true,
+            pin: true,
+        }
+    });
+}
 
 // SLIDER GSAP
 
 const slider = gsap.utils.toArray(".projets__content");
-let tween = gsap.to(slider, {
-    xPercent: -95 * (slider.length - 0),
-    scrollTrigger: {
-        trigger: ".projets__slider",
-        start: "top 20%",
-        pin: true,
-        scrub: 1,
-    },
-});
-
+if (slider.length > 0) {
+    let tween = gsap.to(slider, {
+        xPercent: -95 * (slider.length - 0),
+        scrollTrigger: {
+            trigger: ".projets__slider",
+            start: "top 20%",
+            pin: true,
+            scrub: 1,
+        },
+    });
+}
 
 // MODAL
 
@@ -285,27 +269,72 @@ links.forEach(function (link) {
         event.preventDefault();
         var modalId = link.getAttribute('data-modal');
         var modal = document.getElementById(modalId);
-        modal.style.display = "block";
-        html.classList.add('modal--open');
-        document.body.style.overflow = "hidden";
+        if (modal) {
+            modal.style.display = "block";
+            html.classList.add('modal--open');
+            document.body.style.overflow = "hidden";
+        }
     });
 });
 
 modals.forEach(function (modal) {
     var closeButton = modal.querySelector('.close');
-    closeButton.addEventListener('click', function () {
-        modal.style.display = "none";
-        document.body.style.overflow = "auto";
-        html.classList.remove('modal--open');
-    });
+    if (closeButton) {
+        closeButton.addEventListener('click', function () {
+            modal.style.display = "none";
+            document.body.style.overflow = "auto";
+            html.classList.remove('modal--open');
+        });
+    }
 
     modal.addEventListener('click', function (event) {
         if (event.target === modal) {
             modal.style.display = "none";
             document.body.style.overflow = "auto";
-
+            html.classList.remove('modal--open');
         }
     });
 });
 
+// SLIDER
 
+const initializeSlider = (slider) => {
+    const sliderList = slider.querySelector('.slider__list');
+    const sliderElements = slider.querySelectorAll('.slider__el');
+    const prevButton = slider.querySelector('.slider__prev');
+    const nextButton = slider.querySelector('.slider__next');
+
+    let currentIndex = 0;
+    const totalSlides = sliderElements.length;
+
+    const goToSlide = (index) => {
+        sliderList.style.transform = `translateX(-${index * 100}%)`;
+        currentIndex = index;
+    };
+
+    const nextSlide = () => {
+        currentIndex = (currentIndex + 1) % totalSlides;
+        goToSlide(currentIndex);
+    };
+
+    const prevSlide = () => {
+        currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+        goToSlide(currentIndex);
+    };
+
+    if (nextButton && prevButton) {
+        nextButton.addEventListener('click', nextSlide);
+        prevButton.addEventListener('click', prevSlide);
+    }
+
+    setInterval(nextSlide, 5000);
+
+    window.addEventListener('resize', () => {
+        goToSlide(currentIndex);
+    });
+};
+
+const sliders = document.querySelectorAll('.slider');
+sliders.forEach((slider) => {
+    initializeSlider(slider);
+});
